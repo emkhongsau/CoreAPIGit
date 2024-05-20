994b5a81436bb72cb0a67feddb6816a98480c2b9
Thanh Duy <ntduy61@gmail.com>
2024-05-20 10:33:32 +0700

    create models

---
+++ CoreAPIGit/Models/MessagechatModel.cs
@@ -0,0 +1,16 @@
+﻿namespace CoreAPIGit.Models
+{
+    public class MessagechatModel
+    {									
+        public Guid ChatID  { get; set; }
+        public Guid SessionID { get; set; }
+        public long FromUser { get; set; }
+        public long ToUser { get; set; }
+        public string MessageChat { get; set; }
+        public string MessageChatUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int IsWatched { get; set; }
+        public DateTime DateWatched { get; set; }
+        public int SequenceNumber { get; set; }
+    }
+}
